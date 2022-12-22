8bc086890df7307be32a06bc81022623a09dd0a1
Alexander <akrimec@gmail.com>
2022-12-22 02:36:44 +0200

    sprint03 done

--- sprint03/task03.py
+++ sprint03/task03.py
@@ -8,6 +8,7 @@ def create_account(user_name: str, password: str, secret_words: list):
         raise ValueError
     if len(password) < 6:
         raise ValueError
+
     def check(pas, words):
         counter = 0
         s = [x for x in words]
@@ -30,21 +31,8 @@ def create_account(user_name: str, password: str, secret_words: list):
         return k
     return check
 
-
-user2 = create_account("User2", "yu6r*Tt5", ["word1", "abc3", "list"])
-print(user2("yu6r*Tt5", ["word1", "zzzz", "z"]))
-
-# tom = create_account("Tom", "Qwerty1_", ["1", "word"])
-# print(tom("Qwerty1_", ["1", "word"]))
-# print(tom("Qwerty1_", ["word"]))
-# print(tom("Qwerty1_", ["word", "2"]))
-# print(tom("Qwerty1!", ["word", "12"]))
-#
-#
-# val1 = create_account("123", "qQ1!45", ["1", "word"])
-# print(val1("qQ1!45", ["1", "word"]))
-
-try:
-    simple_user = create_account("A", "Aa!1", ["word"])
-except ValueError:
-    print("Raises ValueError")
+tom = create_account("Tom", "Qwerty1_", ["1", "word"])
+print(tom("Qwerty1_", ["1", "word"]))
+print(tom("Qwerty1_", ["word"]))
+print(tom("Qwerty1_", ["word", "2"]))
+print(tom("Qwerty1!", ["word", "12"]))
